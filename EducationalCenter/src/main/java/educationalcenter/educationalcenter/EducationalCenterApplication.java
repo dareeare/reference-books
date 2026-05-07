9e922c0095274554970152fa715a710a6b690779
“Voitehovskaya <voitehovsk.daria@gmail.com>
2026-05-07 19:24:10 +0300

    1

---
+++ EducationalCenter/src/main/java/educationalcenter/educationalcenter/EducationalCenterApplication.java
@@ -0,0 +1,13 @@
+package educationalcenter.educationalcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationalCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducationalCenterApplication.class, args);
+    }
+
+}
