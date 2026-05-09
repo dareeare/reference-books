e5de468743507f77005ee98e63595dfa6d6dd2df
“Voitehovskaya <voitehovsk.daria@gmail.com>
2026-05-09 18:21:32 +0300

    example of data

--- EducationalCenter/src/main/java/educationalcenter/educationalcenter/entity/Teacher.java
+++ EducationalCenter/src/main/java/educationalcenter/educationalcenter/entity/Teacher.java
@@ -11,6 +11,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+@Table(name = "Teachers")
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
---
+++ data_example.sql
@@ -0,0 +1,9 @@
+INSERT INTO Teachers (last_name, first_name, patronymic, specialization, hire_date, hourly_rate, experience)
+VALUES ('Иванов', 'Петр', 'Сергеевич', 'Разработка на Java', '2025-07-05', 6.00, 2),
+       ('Моисеенко', 'Татьяна', 'Антоновна', 'SQL разработчик', '2025-09-12', 5.00, 1),
+       ('Вивьенов', 'Михаил', 'Петрович', 'Компьютерные сети', '2025-02-06', 7.00, 3);
+
+INSERT INTO Courses (course_name, teacher_id, start_date, course_cost, total_hours)
+VALUES ('Основы разработки на Java', 1, '2026-05-05', 1000, 40),
+       ('Продвинутая Java', 1, '2026-05-25', 1500, 70),
+       ('Базы данных', 2, '2026-06-01', 700, 30);
